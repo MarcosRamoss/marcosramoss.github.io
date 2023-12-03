84fe971d1160cd2905cfdddcde8fff0a49d67a3d
Marcos Ramos <mksvncs@gmail.com>
2023-12-03 10:29:54 +0000

    melhorei o menu mobile

--- js/main.js
+++ js/main.js
@@ -12,20 +12,38 @@ links.forEach(ativarLink);
 
 // Ativar botão menu hamburger
 const btnMenu = document.querySelector("#btn-mobile");
-function toggleMenu() {
+function toggleMenu(e) {
+  if (e.type === "touchstart") e.preventDefatul();
   const nav = document.querySelector("#nav");
   nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  e.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    e.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    e.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+  if (nav.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
 }
 btnMenu.addEventListener("click", toggleMenu);
+btnMenu.addEventListener("touchstart", toggleMenu);
 
 // Animação
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
 
-function fixarMenu(){
-  let header = document.querySelector('#header')
-  header.classList.toggle('fixar', window.scrollY > 0)
+function fixarMenu() {
+  let header = document.querySelector("#header");
+  header.classList.toggle("fixar", window.scrollY > 0);
 }
-
-window.addEventListener('scroll', fixarMenu)
+function ajustarMenu() {
+  const menuMobile = document.querySelector("#menu");
+  menuMobile.classList.toggle("ajustar", window.scrollY > 0);
+}
+window.addEventListener("scroll", fixarMenu);
+window.addEventListener("scroll", ajustarMenu);
